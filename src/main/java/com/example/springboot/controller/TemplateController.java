abcf7e360aee64d3e98e76b0931753202f1f3695
GM <itreflectdream@gmail.com>
2017-09-12 22:58:09 +0800

    测试

--- src/main/java/com/example/springboot/controller/TemplateController.java
+++ src/main/java/com/example/springboot/controller/TemplateController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @Controller
 public class TemplateController {
     /**
-     * 返回thymleaf html模板,测试git
+     * 返回thymeleaf模板
      */
     @RequestMapping("/helloHtml")
     public String helloHtml(Map<String,Object> map){
